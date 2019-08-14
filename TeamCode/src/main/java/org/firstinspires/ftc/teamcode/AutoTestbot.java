d43dded1698ac75f811d6c90eacfa2014d2b746e
Kevin Mathew <thekevinmathew@gmail.com>
2019-08-14 01:49:57 -0500

    added vuforia

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTestbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTestbot.java
@@ -29,7 +29,6 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -38,27 +37,48 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+import java.util.List;
 
 @Autonomous(name="TestOfPID", group="testbot")
 //@Disabled
 public class AutoTestbot extends LinearOpMode {
 
     /* Declare OpMode members. */
-    GyroMath                gyro    = new GyroMath();
-    HardwareTestbot         robot   = new HardwareTestbot();   // Use a Pushbot's hardware
+    GyroMath gyro = new GyroMath();
+    HardwareTestbot robot = new HardwareTestbot();   // Use a Pushbot's hardware
 
     double PID_power;
+
+    //vuforia power
+    private static final String LABEL_GOLD_MINERAL = "Gold Mineral";
+    private static final String VUFORIA_KEY = " -- YOUR NEW VUFORIA KEY GOES HERE  --- ";
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
+
     @Override
     public void runOpMode() {
         robot.init(hardwareMap);
         // Ensure the robot it stationary, then reset the encoders and calibrate the gyro.
         robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //start up Vuforia Engine
+        initVuforia();
+        if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
+            initTfod();
+        } else {
+            telemetry.addData("Sorry!", "This device is not compatible with TFOD");
+        }
         // Send telemetry message to alert driver that we are calibrating;
         telemetry.addData(">", "Calibrating Gyro");    //
         telemetry.update();
         //setting target later changable
-        gyro.target_Angle = (gyro.angle.firstAngle+360)%360;
+        gyro.target_Angle = (gyro.angle.firstAngle + 360) % 360;
         telemetry.addData(">", "Target Angle Set");    //
         telemetry.update();
         //confirm
@@ -74,18 +94,49 @@ public class AutoTestbot extends LinearOpMode {
         }
 
         waitForStart();
-        while(isStarted()){
+        while (isStarted()) {
             PID_power = gyro.calcAngle();
             robot.leftDrive.setPower(PID_power);
             robot.rightDrive.setPower(PID_power);
             telemetry.addLine("Robot Error = %d" + gyro.angle_error);
             telemetry.addLine("Robot Heading = %d" + gyro.getAngle());
             telemetry.update();
+            //block target calcs
+            if (tfod != null) {
+                tfod.activate();
+            }
+            if (tfod != null) {
+                List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                if (updatedRecognitions != null) {
+                    telemetry.addData("# Object Detected", updatedRecognitions.size());
+                    if (updatedRecognitions.size() == 1) {
+                        int goldMineralX = -1;
+                        for (Recognition recognition : updatedRecognitions) {
+                            if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
+                                goldMineralX = (int) recognition.getLeft();
+                            }
+                        }
+                    }
+                }
+            }
+            if (tfod != null) {tfod.shutdown(); }
+            telemetry.addData("Path", "Complete");
+            telemetry.update();
         }
-
-        telemetry.addData("Path", "Complete");
-        telemetry.update();
+    }
+    //just vuforia engine starting
+    private void initVuforia() {
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
     }
 
-
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(LABEL_GOLD_MINERAL);
+    }
 }
+
