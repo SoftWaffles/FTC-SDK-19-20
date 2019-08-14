5890b02511d69f3c17efa4209fafd06d9236ae00
Kevin Mathew <thekevinmathew@gmail.com>
2019-08-14 01:27:51 -0500

    Updated Gradle Added PID Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTestbot.java
@@ -0,0 +1,86 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+
+@Autonomous(name="TestOfPID", group="testbot")
+//@Disabled
+public class AutoTestbot extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    GyroMath                gyro    = new GyroMath();
+    HardwareTestbot         robot   = new HardwareTestbot();   // Use a Pushbot's hardware
+
+    double PID_power;
+    @Override
+    public void runOpMode() {
+        robot.init(hardwareMap);
+        // Ensure the robot it stationary, then reset the encoders and calibrate the gyro.
+        robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        // Send telemetry message to alert driver that we are calibrating;
+        telemetry.addData(">", "Calibrating Gyro");    //
+        telemetry.update();
+        telemetry.addData(">", "Robot Ready.");    //
+        telemetry.update();
+
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        while (!isStarted()) {
+            telemetry.addData(">", "Robot Heading = %d", gyro.getAngle());
+            telemetry.update();
+        }
+
+        waitForStart();
+        while(isStarted()){
+            PID_power = gyro.calcAngle();
+            robot.leftDrive.setPower(PID_power);
+            robot.rightDrive.setPower(PID_power);
+            telemetry.addLine("Robot Error = %d" + gyro.angle_error);
+            telemetry.addLine("Robot Heading = %d" + gyro.getAngle());
+            telemetry.update();
+        }
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+    }
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroMath.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+
+public class GyroMath {
+    //define class members
+    ElapsedTime runtime = new ElapsedTime();
+    public HardwareTestbot myRobot;
+
+    //declaration of objects
+    public Orientation angle;
+    public Acceleration gravity;
+
+    //angle variables
+    public double globalAngle;
+    double target_Angle = 0;
+    double prev_angle_error, angle_error;
+
+    //distance variables
+    double distance = 0;
+    double prev_dist_error, dist_error;
+    double target_Distance = 1;
+
+    //timing
+    double elaspsedTime, time, timePrev;
+    double period;
+
+    //variables for the PID systems
+    double kP = 1;
+    double kI = 0.2;
+    double kD = 10;
+
+    //actual PID outputs
+    double PID_p, PID_i, PID_d, PID_total;
+
+    public GyroMath(){
+    }
+
+    public void init(HardwareTestbot robot){
+        myRobot = robot;
+        time = runtime.seconds();
+    }
+    void resetAngle()
+    {
+        angle = myRobot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        globalAngle = 0;
+    }
+    double getAngle()
+    {
+        Orientation angle = myRobot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        globalAngle = (angle.firstAngle+360)%360;
+        return globalAngle;
+    }
+    public double calcAngle(){
+        if(runtime.seconds() > time + period){
+            time = runtime.seconds();
+            angle_error = target_Angle - getAngle();
+            PID_p = kP * angle_error;
+            double angle_Derv = angle_error - prev_angle_error;
+            PID_d = kD*(angle_Derv/period);
+
+            if(-50 < angle_error && angle_error < 50){
+                PID_i = PID_i + (kI * angle_error);
+            }else{
+                PID_i = 0;
+            }
+            PID_total = PID_p + PID_i + PID_d;
+            prev_angle_error = angle_error;
+            //send back value
+        }
+        return PID_total;
+    }
+
+}
