b4c0647a9cdcac5b0a2a74955970725fa1083c4a
Kevin Mathew <thekevinmathew@gmail.com>
2019-09-12 00:54:31 -0500

    merged with 5.2 Skystone

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTestbotVuforia.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTestbotVuforia.java
@@ -26,7 +26,9 @@ public class AutoTestbotVuforia extends LinearOpMode {
     private double PID_power;
     private int prevTarget;
     //vuforia power
-    private static final String LABEL_GOLD_MINERAL = "Gold Mineral";
+    private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
+    private static final String LABEL_FIRST_ELEMENT = "Stone";
+    private static final String LABEL_SECOND_ELEMENT = "Skystone";
     private static final String VUFORIA_KEY = "AZjoEnH/////AAABmXdFD2Xsrk4krInf+EdRY0NaRrdzvbptLaUoVN2kuF2/FnuWVscRF9ozak4bIpJCr1SLehfzrXHS+H3Z7XMNIgxwg6lttQ4zp7ODEDt1XQ/DLQcjpmYXruF4eBBRsIBey35Ue6g4E51WOebmNW/aDFDhz3zON+NNYbyk/4XOszsw7CwHpcNLBXqT0prM/NYwkCaJFocA8cpWcViM0Mka8kEV+T1X1ZtRnPwMxtQrxO19ksdbRv0bjPmco0iiOAvRwMcyVxg250tckD64iSWJkIhlqakYMLA1r00YPtUY4VSfShG0pWTDn/RF9/TqhM8qICp9ZPCz5QlPn8qt4cfiofTjzE41R+VvjKnIGK1B9g5o";
     private VuforiaLocalizer vuforia;
     private TFObjectDetector tfod;
@@ -35,36 +37,31 @@ public class AutoTestbotVuforia extends LinearOpMode {
     public void runOpMode() {
         robot.initDrive(this);
         gyro.initDrive(robot);
-        //start up Vuforia Engine
         initVuforia();
         if (ClassFactory.getInstance().canCreateTFObjectDetector()) { initTfod(); }
         else { telemetry.addData("Sorry!", "This device is not compatible with TFOD"); }
         while (!isStarted()) { telemetry.addData(">", "Robot Heading = %d", gyro.getAngle());telemetry.update(); }
         waitForStart();
-        while (isStarted()) {
-            telemetry.addLine("Robot Heading = %d" + gyro.getAngle());
-            telemetry.update();
-            //block target calcs
-            if (tfod != null) { tfod.activate();}
-            if (tfod != null) {
-                List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-                if (updatedRecognitions != null) {
-                    telemetry.addData("# Object Detected", updatedRecognitions.size());
-                    if (updatedRecognitions.size() == 1) {
-                        int goldMineralX = -1;
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                if (tfod != null) {
+                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                    if (updatedRecognitions != null) {
+                        telemetry.addData("# Object Detected", updatedRecognitions.size());
+                        int i = 0;
                         for (Recognition recognition : updatedRecognitions) {
-                            if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
-                                goldMineralX = (int) recognition.getLeft();
-                                gyroOrien(visionCalc(goldMineralX));
-                            }
+                            telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                            telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                    recognition.getLeft(), recognition.getTop());
+                            telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                    recognition.getRight(), recognition.getBottom());
                         }
+                        telemetry.update();
                     }
                 }
             }
-            if (tfod != null) {tfod.shutdown(); }
-            telemetry.addData("Path", "Complete");
-            telemetry.update();
         }
+        if (tfod != null) { tfod.shutdown(); }
     }
     private int visionCalc(int objX){
         int target = 0;
@@ -92,10 +89,12 @@ public class AutoTestbotVuforia extends LinearOpMode {
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
     }
     private void initTfod() {
-        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minimumConfidence = 0.8;
         tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
-        tfod.loadModelFromAsset(LABEL_GOLD_MINERAL);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
     }
 }
 
